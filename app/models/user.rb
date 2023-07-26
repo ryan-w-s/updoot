ebdb4dab4f15836794b1799f347f5c313d359c39
Ryan Stolliker <ryan.stolliker@gmail.com>
2023-07-25 17:26:00 -0700

    Authentication/Users finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: {maximum: 50}, uniqueness: true, format: {with: /\A[a-zA-Z0-9]+\Z/}
 end
