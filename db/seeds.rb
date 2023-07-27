968955a7f5122ef8ab071785e14d237d0e2aad20
Ryan Stolliker <ryan.stolliker@gmail.com>
2023-07-27 01:12:58 -0700

    Navbar Login/Logout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ryan = User.create! :name => 'ryanadmin', :email => 'ryan.stolliker@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
