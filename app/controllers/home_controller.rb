b8ad7cc0e6ccba56bc795c5b8a475c0eddc20ab9
Ryan Stolliker <ryan.stolliker@gmail.com>
2025-01-13 02:55:53 -0800

    Home page feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    base_query = Post.includes(:user, :collection, :doots)
+                    .where('posts.created_at > ?', 1.week.ago)
+                    .left_joins(:doots)
+                    .group('posts.id')
+                    .order('COUNT(doots.id) DESC, posts.created_at DESC')
+
+    if user_signed_in?
+      @posts = base_query.joins(collection: :watchers)
+                        .where(watchers: { user_id: current_user.id })
+    else
+      @posts = base_query
+    end
+
+    @posts = @posts.page(params[:page]).per(10)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,9 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  # Add Kaminari pagination
+  paginates_per 10
+
   def doot_total
     doots.sum(:value)
   end
